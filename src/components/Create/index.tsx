8a038b3b3ad76a1c698537a0f71680e39a7c49d6
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-08-28 17:35:37 +0000

    new bug added

--- src/components/Create/index.tsx
+++ src/components/Create/index.tsx
@@ -7,10 +7,61 @@ import {
   Container,
   Button,
 } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import { useSupabase } from '../../utils/supabase';
 
 export function CreateExplore() {
   const navigation = useNavigate()
+  const supabase = useSupabase();
+  const form = useForm({
+    initialValues: {
+      username: '',
+    }
+  })
+
+
+  const createRoom = async (values: any) => {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" }).then(async function (result) {
+          if (result.state === "granted") {
+            const { data: room, error } = await supabase.from('room').insert([{}]).select('id').single()
+            if (error) {
+              throw new Error(error.message)
+            }
+            const room_id = room?.id
+
+            return room_id
+          } else {
+            alert("Please enable location access to continue")
+          }
+        }).catch(function (error) {
+          throw new Error(error)
+        });
+    } else {
+      throw new Error('Geolocation is not supported by this browser.')
+    }
+  }
+
+  const {
+    isLoading,
+    mutate: createUserAndRoom,
+  } = useMutation(createRoom, {
+    onSuccess: (data) => {
+      console.log(data)
+    },
+    onError: (error) => {
+      console.error(error)
+    }
+  })
+
   return (
     <Container size={420} my={40}>
       <Title
@@ -30,10 +81,19 @@ export function CreateExplore() {
       </Text>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Username" placeholder="Peter Griffin" required />
-        <Button fullWidth mt="xl">
-          Create
-        </Button>
+        <form
+          onSubmit={form.onSubmit(value => createUserAndRoom(value))}
+        >
+          <TextInput
+            {...form.getInputProps('username')}
+            label="Username" placeholder="Peter Griffin" required />
+          <Button
+            type='submit'
+            loading={isLoading}
+            fullWidth mt="xl">
+            Create
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,19 @@ import App from './App'
 import './index.css'
 import { SupabaseProvider } from './utils/supabase'
 import { MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SupabaseProvider>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={{
-        colorScheme: "dark",
-        fontFamily: "Poppins",
-      }}>
-        <App />
-      </MantineProvider>
-    </SupabaseProvider>
+    <QueryClientProvider client={queryClient}>
+      <SupabaseProvider>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={{
+          colorScheme: "dark",
+          fontFamily: "Poppins",
+        }}>
+          <App />
+        </MantineProvider>
+      </SupabaseProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
